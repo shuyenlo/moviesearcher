6bd50ba686a4caab4f313fa34f942dcc6eb31012
羅書硯 <luoshuyan@luoshuyande-iMac.local>
2020-04-02 10:57:57 +0800

    Updates

---
+++ precache-manifest.3e25e7918cbb0a82f320ec9048735263.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93139e4c2c4a27ed3ae7e9974683b86",
+    "url": "/moviesearcher/index.html"
+  },
+  {
+    "revision": "498e81eebb6ebd64096e",
+    "url": "/moviesearcher/static/css/main.aea41a92.chunk.css"
+  },
+  {
+    "revision": "8bc6253b20f49b6b323b",
+    "url": "/moviesearcher/static/js/2.6b6e808a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviesearcher/static/js/2.6b6e808a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "498e81eebb6ebd64096e",
+    "url": "/moviesearcher/static/js/main.8c936cd4.chunk.js"
+  },
+  {
+    "revision": "2cb2eb0181e7fb1d9443",
+    "url": "/moviesearcher/static/js/runtime-main.eac3c2be.js"
+  },
+  {
+    "revision": "44a4dbceb6963f31791057ed3db15939",
+    "url": "/moviesearcher/static/media/no-image.44a4dbce.jpg"
+  }
+]);
